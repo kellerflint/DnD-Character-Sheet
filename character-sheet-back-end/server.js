edc280358b26fed7c10032984aee1c20c14fe903
TavSingh <tavparsadsingh2@gmail.com>
2025-09-28 14:42:39 -0700

    Added basic api check

--- character-sheet-back-end/server.js
+++ character-sheet-back-end/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 // Short hand to load variables from .env file
 require("dotenv").config();
 
 const app = express();
+
+// Adding this information since we will be using multiple api connections
+// We will use a set that we can use to check the different apis
+// For now we will check a basic api connection
+// const BASE_API = 'api.open5e.com';
+// const classInfo = new Set(['races', 'classes', 'backgrounds']);
 
 // Uses the PORT from .env or defaults to 3001
 const PORT = process.env.PORT || 3001;
@@ -14,11 +21,17 @@ app.use(cors());
 app.use(express.json());
 
 // Back-end route health check - using /api to differentiate from front-end routes (if any are added later)
-app.get("/api/check", (req, res) => {
-   res.send("Hello from the back-end side!");
-})
+app.get("/api/check", async (req, res) => {
+   try {
+      const response = await axios.get('https://api.open5e.com/v2/documents/');
+      res.json(response.data);
+   } catch (error) {
+      console.error('Error, cannot connect and fetch from external API: ', error);
+      res.status(500).json({message: 'Error fetching data'});
+   }
+});
 
 // Start the server
 app.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
