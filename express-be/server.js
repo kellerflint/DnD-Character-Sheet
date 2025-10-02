f56e5ecab1874180902402402ab081929754cc20
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-10-02 11:38:37 -0700

    Added listening port for VM database connection (attempting to connect local MySQL Workbench to the database VM.)

--- express-be/server.js
+++ express-be/server.js
@@ -7,3 +7,7 @@ app.use("/", router);
 
 const port = 3001;
 app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
+
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+    console.log('Server up');
+});
