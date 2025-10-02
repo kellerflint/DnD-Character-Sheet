3e080b88e0e66f380f7287ad7fcef538b69acc47
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-10-02 11:47:49 -0700

    Added some things to the server.js file utilizing the dnd api that Tav gave us. Installed a few packages inside of the server folder, make sure to cd into that folder and do an npm i.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import morgan from 'morgan';
+import axios from 'axios';
 
 dotenv.config({
-
+    path: '/.env'
 });
+
+const OPEN5E_BASE = process.env.OPEN5E_BASE || 'https://api.open5e.com';
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(morgan());
+
+const PORT = process.env.PORT || 3001;
+
+const characterInfo = new Set("level", "class", "race", "")
 
