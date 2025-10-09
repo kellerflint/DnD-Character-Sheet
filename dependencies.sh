7e269a634186ff76e825b35407eeabc0778da85f
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-10-09 11:44:38 -0700

    Edited the shell script.

--- dependencies.sh
+++ dependencies.sh
@@ -11,3 +11,18 @@ yes | sudo apt install git
 yes | sudo apt install mysql-server
 yes | sudo apt install nodejs npm
 
+# You'll need to put the github repo link between the single quotes and uncomment before running this script 
+# git clone 'github repo link'
+
+cd DnD-Character-Sheet
+cd frontend/dnd-character-sheet
+
+npm install
+
+cd ../../server
+
+npm install
+npm i -g nodemon
+
+cd ..
+
