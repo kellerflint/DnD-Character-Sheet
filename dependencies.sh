2de5624d8afd27a5539fae33ace3f80ac7349edf
= <hellohi12465@gmail.com>
2025-10-09 23:38:51 -0700

    Made the script echo some text output for which stage it is on.

--- dependencies.sh
+++ dependencies.sh
@@ -4,22 +4,32 @@
 #Author: Raymond Marx
 
 #Auto-upgrade
+echo "Auto Upgrade"
+
 yes | sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq upgrade
 
 #Put in password here
+echo "Input SUDOER Password"
+
 sudo -v
 
 # Autoinstall dependencies
+echo "Install apt packages"
+
 yes | sudo apt install git
 yes | sudo apt install mysql-server
 yes | sudo apt install nodejs npm
 
 
 # Install frontend node dependencies
+echo "Install front-end dependencies"
+
 cd frontend/dnd-character-sheet/
 npm install react react-dom next
 
 # Install backend node dependencies
+echo "Install backend depdendencies"
+
 cd ../..
 cd server
 npm install express dotenv #Important node stuff
