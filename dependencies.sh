046fdc4e6c4b3d49744a87200f7831eaa6a800a8
= <hellohi12465@gmail.com>
2025-10-10 00:21:31 -0700

    Added a node auto-update to dependencies.sh. I'm upgrading node to v22.0.0 to hopefully fix the VM.

--- dependencies.sh
+++ dependencies.sh
@@ -3,15 +3,15 @@
 #Installs dependencies for VM setup. Run this as needed to start off or reinstall/update dependencies.
 #Author: Raymond Marx
 
-#Auto-upgrade
-echo "Auto Upgrade"
-
-yes | sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq upgrade
-
 #Put in password here
 echo "Input SUDOER Password"
 
 sudo -v
+
+#Auto-upgrade
+echo "Auto Upgrade"
+
+yes | sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq upgrade
 
 # Autoinstall dependencies
 echo "Install apt packages"
@@ -20,6 +20,14 @@ yes | sudo apt install git
 yes | sudo apt install mysql-server
 yes | sudo apt install nodejs npm
 
+# Auto-update node to v22.0.0
+echo "Upgrading to Node v22.0.0"
+
+sudo npm install -g n
+sudo n 22.0.0
+
+hash -r #resets location hash of node (/usr/bin/node -> /usr/local/bin/node)
+node -v #Show updated version
 
 # Install frontend node dependencies
 echo "Install front-end dependencies"
