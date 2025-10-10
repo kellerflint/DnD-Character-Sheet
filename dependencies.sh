2fe8ee4d529f2478fffd85f306bb14c3bfb4434f
= <hellohi12465@gmail.com>
2025-10-09 23:30:28 -0700

    Added npm dependencies to autoinstall in dependencies.sh
    Will need to start getting server and FE up and running ASAP!

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#Installs dependencies for VM setup.
+#Installs dependencies for VM setup. Run this as needed to start off or reinstall/update dependencies.
 #Author: Raymond Marx
+
+#Auto-upgrade
+yes | sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq upgrade
 
 #Put in password here
 sudo -v
@@ -10,4 +13,20 @@ sudo -v
 yes | sudo apt install git
 yes | sudo apt install mysql-server
 yes | sudo apt install nodejs npm
+
+
+# Install frontend node dependencies
+cd frontend/dnd-character-sheet
+npm install react react-dom next
+
+# Install backend node dependencies
+cd ../..
+cd backend
+npm install express dotenv #Important node stuff
+npm install cors axios #Connectivity
+npm install sequelize #DB
+npm install morgan #Miscellaneous
+
+
+
 
