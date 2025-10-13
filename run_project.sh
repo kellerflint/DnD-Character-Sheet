953c42fd286bf81a3f30a57be2854ed6d7811516
= <hellohi12465@gmail.com>
2025-10-12 20:17:53 -0700

    Trying to fix line nine where the cd isn't found.

--- run_project.sh
+++ run_project.sh
@@ -6,5 +6,5 @@
 git pull 'https://github.com/tgillysuit/DnD-Character-Sheet.git' #Iffy if we have multiple branches, but good for now.
 
 # Run the frontend the way I did on the VM. 
-(cd /frontend/dnd-character-sheet && node run build && node run start)
+(cd ./frontend/dnd-character-sheet && node run build && node run start)
 
