8532d2c0bfbb8d793b4ca3e5a43cf855424ab6e8
HumaGitGud <khomidov.huma@student.greenriver.edu>
2025-10-12 22:20:55 -0700

    improved home page css

--- react-fe/src/App.jsx
+++ react-fe/src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+// src/App.jsx
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CharactersPage from './pages/CharactersPage';
