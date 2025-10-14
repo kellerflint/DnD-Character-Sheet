6ba45e8537ddb7be0459bf4592270307f62046ff
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-10-14 11:28:02 -0700

    fixed a bug inside of the connet.js file

--- server/database/connect.js
+++ server/database/connect.js
@@ -5,10 +5,11 @@ dotenv.config({
     path: './.env'
 });
 
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_DATABASE, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(
-    DB_NAME, DB_USER, DB_PASS, {
+    DB_DATABASE, DB_USER, DB_PASS, {
+        database: DB_DATABASE,
         host: DB_HOST,
         port: DB_PORT,
         dialect: 'mysql',
