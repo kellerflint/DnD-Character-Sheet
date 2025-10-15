6a22e3421ad41c3c318cd6bf45334e55e386a852
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-10-14 18:27:37 -0700

    Fixed pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,18 +2,22 @@ module.exports = {
   apps: [
     {
       name: 'dnd-backend',
-      script: './express-be/server.js',
+      script: 'server.js',
       cwd: './express-be',
       instances: 1,
       autorestart: true,
       watch: false,
       env: {
+        NODE_ENV: 'development',
+        PORT: 3001
+      },
+      env_production: {
         NODE_ENV: 'production',
         PORT: 3001
       },
-      error_file: './logs/backend-err.log',
-      out_file: './logs/backend-out.log',
-      log_file: './logs/backend-combined.log',
+      error_file: '../logs/backend-err.log',
+      out_file: '../logs/backend-out.log',
+      log_file: '../logs/backend-combined.log',
       time: true
     },
     {
@@ -25,12 +29,16 @@ module.exports = {
       autorestart: true,
       watch: false,
       env: {
+        NODE_ENV: 'development',
+        PORT: 3000
+      },
+      env_production: {
         NODE_ENV: 'production',
         PORT: 3000
       },
-      error_file: './logs/frontend-err.log',
-      out_file: './logs/frontend-out.log',
-      log_file: './logs/frontend-combined.log',
+      error_file: '../logs/frontend-err.log',
+      out_file: '../logs/frontend-out.log',
+      log_file: '../logs/frontend-combined.log',
       time: true
     }
   ]
