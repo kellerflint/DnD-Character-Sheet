4719e956cbe1c59b8a149f9268d2b53ea6840f44
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-10-15 13:54:28 -0700

    changed a little capitalization for the shell script file.

--- db_setup.sh
+++ db_setup.sh
@@ -54,7 +54,7 @@ FLUSH PRIVILEGES;
 
 USE dnd_character_sheets;
 
-CREATE TABLE Users (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
