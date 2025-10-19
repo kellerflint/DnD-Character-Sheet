6ff4b519c937a5d21b4f78ccebe7b023365a9417
RMarx1456 <hellohi12465@gmail.com>
2025-10-18 20:34:04 -0700

    Implemented update (Allowing the user to update their email).
    A lot of the fixes made for this to happen were bandaid fixes and the localstorage object for the user itself is quote strange, as user details are encapsulated within the 1st level id field, as well as having the same info repeated in the first level as well.

---
+++ frontend/dnd-character-sheet/src/app/user/[username]/edit/page.js
@@ -0,0 +1,14 @@
+// Layout Page for Admin
+'use client'
+
+import { useParams } from 'next/navigation'
+
+import EmailModifier from '@/components/EmailModifier';
+
+export default function UserPage() {
+    
+    return (
+        <EmailModifier />
+    );
+}
+
