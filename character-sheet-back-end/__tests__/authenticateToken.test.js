d107010c6c09b2788a58c44e690468ffd577fdcb
f3liz <felixchen14@gmail.com>
2025-11-04 11:04:37 -0800

    created test case for no token provided for authenticateToken function

--- character-sheet-back-end/__tests__/authenticateToken.test.js
+++ character-sheet-back-end/__tests__/authenticateToken.test.js
@@ -1,2 +1,24 @@
 const jwt = require("jsonwebtoken");
-const authenticateToken = require("..authenticateToken.js");
+const authenticateToken = require("../middleware/authenticateToken.js");
+
+// Mock the tocken
+jest.mock("jsonwebtoken");
+
+describe("authenticateToken middleware function", () => {
+    let req, res, next;
+
+    beforeEach(() => {
+        req = { headers: {} };
+        res = {
+            sendStatus: jest.fn(),
+        }
+        next = jest.fn();
+    });
+
+    test("Returns 401 if no token is provided", () => {
+        req.headers["authorization"] = null;
+        authenticateToken(req, res, next);
+        expect(res.sendStatus).toHaveBeenCalledWith(401);
+        expect(next).not.toHaveBeenCalled();
+    })
+})
