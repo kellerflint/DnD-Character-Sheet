736da0b2972928258898aa46d2920402af2958c9
f3liz <felixchen14@gmail.com>
2025-11-09 10:22:38 +0900

    implemented code to seed db with test data and clear after tests

--- character-sheet-back-end/__tests__/authenticateRoutes.test.js
+++ character-sheet-back-end/__tests__/authenticateRoutes.test.js
@@ -13,3 +13,17 @@ let testingUser = {
 
 let authenticateToken = "";
 
+// will run one time before all the tests
+beforeAll(async () => {
+    await db.query("DELETE FROM users WHERE EMAIL = ?", [testingUser.email]);
+});
+
+// will run once after all tests to clear up test user from test db
+afterAll(async () => {
+    await db.query("DELETE FROM users where email = ?", [testingUser.email]);
+    await db.end();
+});
+
+describe("Integration tests for 3 API Endpoints", () => {
+    
+})
