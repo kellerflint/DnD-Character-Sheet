5abd29e650dac1348ab81df350bce807e67f85fd
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-11-24 11:19:53 -0800

    Attempting to fixed front end unit tests since axios is not using import.meta instead of process.

--- character-sheet-front-end/jest.config.cjs
+++ character-sheet-front-end/jest.config.cjs
@@ -5,7 +5,8 @@ module.exports = {
     },
     moduleNameMapper: {
         "\\.(css|scss|jpg|png)$": "identity-obj-proxy",
-        "^axios$": "<rootDir>/src/__mocks__/axios.js"
+        "^axios$": "<rootDir>/src/__mocks__/axios.js",
+        "^.*/api/axios$": "<rootDir>/src/__mocks__/axiosInstance.js"
     },
     setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
 };
