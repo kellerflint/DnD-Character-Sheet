777e0350aae7549ac2243dd73baecbdd59a788d6
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-11-24 11:34:03 -0800

    Had to update files to account for import.meta since it doesn't natively work with jest

--- character-sheet-front-end/babel.config.cjs
+++ character-sheet-front-end/babel.config.cjs
@@ -2,5 +2,8 @@ module.exports = {
     presets: [
       "@babel/preset-env",
       ["@babel/preset-react", { runtime: "automatic" }]
+    ],
+    plugins: [
+        "transform-import-meta"
     ]
 };
--- character-sheet-front-end/jest.config.cjs
+++ character-sheet-front-end/jest.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
     moduleNameMapper: {
         "\\.(css|scss|jpg|png)$": "identity-obj-proxy",
         "^axios$": "<rootDir>/src/__mocks__/axios.js",
-        "^.*/api/axios$": "<rootDir>/src/__mocks__/axiosInstance.js"
+        "^.*\\/api\\/axios$": "<rootDir>/src/__mocks__/axiosInstance.js",
+        "^\\./axios$": "<rootDir>/src/__mocks__/axiosInstance.js"
     },
     setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
 };
